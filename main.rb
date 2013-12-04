22df9fb715708729ba69e95b1415ab279ca23cf1
Elephant Liu <lexdene@sohu.com>
2013-12-04 18:05:46 +0800

    ImporterScope

--- main.rb
+++ main.rb
@@ -1,9 +1,22 @@
 require 'haml'
 
+class ImporterScope
+  def initialize(filename)
+    @filename = filename
+  end
+
+  def import(path)
+    dirname = File.dirname(@filename)
+    render_file("#{dirname}/#{path}.haml")
+  end
+end
+
 def render_file(filename)
   contents = File.read(filename)
-  engine = Haml::Engine.new(contents, :escape_attrs => false)
-  engine.render(Object.new)
+  engine = Haml::Engine.new(contents,:escape_attrs => false
+)
+  scope = ImporterScope.new(filename)
+  engine.render(scope)
 end
 
 puts render_file('haml/index.haml')
